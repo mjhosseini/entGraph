9bf1cabfe99da0109b6d47f3b9edcffd9c4d0d43
mjhosseini <hosseini.mohammadjavad@gmail.com>
2021-02-19 14:05:46 +0000

    set constants for tokenization info

--- src/constants/ConstantsParsing.java
+++ src/constants/ConstantsParsing.java
@@ -8,21 +8,21 @@ public class ConstantsParsing {
 	public static int numThreads = 60;
 	// final int maxMBallowd = 140;
 	public static boolean convToEntityLinked = false;// Must be always false, we do linking separately!
-	public static String[] accepteds = new String[] { "GE", "EG", "EE", "GG"};//{ "GE", "EG", "EE" }: This is what I've been always using
-	public static boolean onlyNounOrNE = true;//False for TACL experiments and LevyProcessing, true for NC. In order to remove pronouns, etc in case of GG. 
+	public static String[] accepteds = new String[] { "GE", "EG", "EE"};//{ "GE", "EG", "EE" }: This is what I've been always using
+	public static boolean onlyNounOrNE = false;//False for TACL experiments and LevyProcessing, true for NC. In order to remove pronouns, etc in case of GG. 
 	public static final boolean lemmatizePred = true;// eaten.might.1 => eat.might.1// Must be true for normal entailment graphs
-	public static final boolean normalizePredicate = true;// (eat.2, eat.might.1) => (eat.1,eat.2). was false for TACL experiments. It shouldn't change the results, though, since
+	public static final boolean normalizePredicate = false;// (eat.2, eat.might.1) => (eat.1,eat.2). was false for TACL experiments. It shouldn't change the results, though, since
 	//we used to do this in Aggregator: ConstantsAgg.normalizePredicate
-	public static boolean removebasicEvnetifEEModifer = true; //was false for TACL experiments, true for NC.
+	public static boolean removebasicEvnetifEEModifer = false; //was false for TACL experiments, true for NC.
 	public static boolean parseQuestions = false;// Always set it to false for processing the main corpus
-	public static boolean writeUnaryRels = true;
-	public static boolean splitBinary2Unary = true;// if false, the unaries will be the ones that are indeed one arg, e.g., John walked
+	public static boolean writeUnaryRels = false;
+	public static boolean splitBinary2Unary = false;// if false, the unaries will be the ones that are indeed one arg, e.g., John walked
 	public static boolean writeDebugString = true;//should be false when running the experiments
 	public static boolean writeSemParse = true;
 	public static boolean filterUntensed = false;// for deduplicating when a tense is added, eg [receiving; receiving.will]->[receiving.will]
 	public static boolean tenseParseTest = false;// for testing parses on tense pair lists.
 	
-	public static boolean snli = true;//false for TACL experiments, true for NC. a few good hacks for the snli ds
-	public static boolean writeTokenizationInfo = false;
+	public static boolean snli = false;//false for TACL experiments, true for NC. a few good hacks for the snli ds
+	public static boolean writeTokenizationInfo = true;
 	public static int nbestParses = 1;
 }
